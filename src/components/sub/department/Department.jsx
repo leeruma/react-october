9e91640c0e367a26d29b041589a94cc35ab9a346
leeruma <achieve4525@naver.com>
2023-10-04 11:16:41 +0900

    새로운 데이터 추가

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -1,12 +1,13 @@
 import Layout from '../../common/layout/Layout';
 import { useEffect, useState } from 'react';
+const path = process.env.PUBLIC_URL;
 
 export default function Department() {
 	console.log('re-render');
 	const [Department, setDepartment] = useState([]);
 	useEffect(() => {
 		// 해당 useEffect구문은 컴포넌트 마운트시 한번만 동작됨
-		fetch('/DB/department.json')
+		fetch(`${path}/DB/department.json`)
 			.then((data) => data.json())
 			.then((json) => {
 				console.log(json.members);
@@ -18,7 +19,11 @@ export default function Department() {
 			{Department.map((member, idx) => {
 				return (
 					<article key={idx}>
+						<div className='pic'>
+							<img src={`${path}/img/${member.pic}`} alt={member.name} />
+						</div>
 						<h2>{member.name}</h2>
+						<p>{member.position} </p>
 					</article>
 				);
 			})}
