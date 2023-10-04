2c4a166f9ba9afae0739908e3db73f90794eaedb
leeruma <achieve4525@naver.com>
2023-10-04 13:28:51 +0900

    memberBox 스타일

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -45,7 +45,7 @@ export default function Department() {
 				{Department.map((member, idx) => {
 					return (
 						<article key={idx}>
-							<div className='pic'>
+							<div className={styles.pic}>
 								<img src={`${path}/img/${member.pic}`} alt={member.name} />
 							</div>
 							<h2>{member.name}</h2>
